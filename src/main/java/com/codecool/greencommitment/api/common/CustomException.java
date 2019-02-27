731824b334d291996fbbdfff2df8291f3997e797
Berta Vincze <bertavincze@gmail.com>
2019-02-27 20:19:59 +0100

    Added new CustomException class

---
+++ src/main/java/com/codecool/greencommitment/api/common/CustomException.java
@@ -0,0 +1,8 @@
+package com.codecool.greencommitment.api.common;
+
+public class CustomException extends Exception {
+
+    public CustomException (String message) {
+        super(message);
+    }
+}
