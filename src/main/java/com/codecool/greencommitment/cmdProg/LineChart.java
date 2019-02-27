cbcedcff5fee6a779784ca1c90eebbca24e9774d
Toth-Gabor <upcgabor@gmail.com>
2019-02-27 13:31:57 +0100

    lineChart time format

--- src/main/java/com/codecool/greencommitment/cmdProg/LineChart.java
+++ src/main/java/com/codecool/greencommitment/cmdProg/LineChart.java
@@ -1,6 +1,8 @@
 package com.codecool.greencommitment.cmdProg;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.codecool.greencommitment.api.common.Measurement;
 import com.codecool.greencommitment.api.common.MeasurementType;
@@ -15,9 +17,9 @@ public class LineChart {
     
     public static void main( String[ ] args ) throws Exception {
         LineChart lc = new LineChart();
-        lc.showLineChart("joli","buli");
+        lc.showLineChart("joli","line");
     }
-    public void showLineChart(String id, String type) throws InterruptedException, IOException {
+    public void showLineChart(String id, String type) throws InterruptedException, IOException, IllegalArgumentException {
         DefaultCategoryDataset line_chart_dataset = new DefaultCategoryDataset();
         Measurement m = new Measurement(12,12, MeasurementType.CELSIUS);
         Thread.sleep(1000);
@@ -28,14 +30,16 @@ public class LineChart {
         Measurement m3 = new Measurement(18,45, MeasurementType.CELSIUS);
         Thread.sleep(1000);
         Measurement m4 = new Measurement(99,66, MeasurementType.CELSIUS);
-    
-        line_chart_dataset.addValue(m.getValue(),"temp", String.valueOf(m.getTime()));
-        line_chart_dataset.addValue(m1.getValue(),"temp", String.valueOf(m1.getTime()));
-        line_chart_dataset.addValue(m2.getValue(),"temp", String.valueOf(m2.getTime()));
-        line_chart_dataset.addValue(m3.getValue(),"temp", String.valueOf(m3.getTime()));
-        line_chart_dataset.addValue(m4.getValue(),"temp", String.valueOf(m4.getTime()));
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date();
+        line_chart_dataset.addValue(m.getValue(),"temp", sdf.format(m.getTime()));
+        line_chart_dataset.addValue(m1.getValue(),"temp", sdf.format(m1.getTime()));
+        line_chart_dataset.addValue(m2.getValue(),"temp", sdf.format(m2.getTime()));
+        line_chart_dataset.addValue(m3.getValue(),"temp", sdf.format(m3.getTime()));
+        line_chart_dataset.addValue(m4.getValue(),"temp", sdf.format(m4.getTime()));
         
         XMLHandler xml = new XMLHandler();
+        
         JFreeChart chart;
         if (type.equalsIgnoreCase("line")){
             chart = ChartFactory.createLineChart(
@@ -43,14 +47,14 @@ public class LineChart {
                 "Temperature",
                 line_chart_dataset,PlotOrientation.VERTICAL,
                 true,true,false);
-        } else {
+        } else if (type.equalsIgnoreCase("bar")){
             chart = ChartFactory.createBarChart("Time & Temperature","Time",
                 "Temperature",
                 line_chart_dataset,PlotOrientation.VERTICAL,
                 true,true,false);
+        } else {
+            throw new IllegalArgumentException("Not existing type of char!");
         }
-        
-        
         
         int width = 640;    /* Width of the image */
         int height = 480;   /* Height of the image */
